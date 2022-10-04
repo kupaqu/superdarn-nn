9dc50361234c46589f3fc43251b8105271de3f83
Буда Вампилов <budavampilov@MacBook-Air-Buda.local>
2022-10-05 01:59:17 +0800

    сделал приведение к регулярному разрешению

--- DataLoader.py
+++ DataLoader.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import PUSHORT
 import os
 import numpy as np
 import datetime as dt
@@ -7,111 +8,190 @@ import bz2
 import pydarn
 
 class DataLoader:
+
     def __init__(self,
-                    datapath: str, # путь к данным в формате fitacf
-                    nrang: int, # количество гейтов
-                    keys: list, # каналы данных
-                    total_bms: int, # количество лучей
-                    total_chs: int, # количество каналов
-                    delta_hours: int = 2, # сдвиг по времени в fitacf файлах
-                    reg_res: int = 60, # регулярное разрешение в данных (обычно 2 минуты)
-                    windows_num: int = 30 # количество периодов
-                    ):
-        self.datapath = datapath
-        self.nrang = nrang
-        self.keys = keys
-        self.total_bms = total_bms
-        self.total_chs = total_chs
-        self.delta_hours = delta_hours
-        self.reg_res = reg_res
-        self.windows_num = windows_num
-    
+                 datapath: str,
+                 windows_num: int = 30):
+
+        self.datapath = datapath        # путь к данным в формате fitacf.bz2
+        self.windows_num = windows_num  # количество периодов в окне
+        self.delta_hours = 2            # разница в часах между началом записи в файле 
+        self.nrang = 75                 # количество гейтов
+        self.keys = ['pwr0',            # ключи данных
+                     'v',
+                     'p_l',
+                     'p_s',
+                     'w_l',
+                     'w_s']
+        self.mask_key = 'qflg'          # ключ по которому лежит маска
+        self.total_bms = 16             # количество лучей
+        self.total_chs = 2              # количество каналов
+        self.reg_res = 60               # регулярное разрешение (количество записей за два часа)
+
     def __iter__(self):
-        for radar_subdir, _, _ in os.walk(self.datapath): # итерация по подпапкам данных каждого из радаров
-            for hour in range(0, 24, self.delta_hours): # итерация по периодам
-                filenames = self.__get_filenames(radar_subdir, hour) # вытаскиваем все имена файлов за период указаный в переменной hour
-                for i in range(len(filenames)-self.windows_num+1): # итерация скользящим окном по файлам
-                    for channel in range(self.total_chs): # итерация по номерам каналов
-                        for beam in range(self.total_bms): # итерация по номерам лучей
-                            yield self.__get_data(filenames=filenames[i:(i+self.windows_num)], # извлечение данных в виде многомерного массива
+
+    #
+    #   Функция вызывающаяся при каждой итерации.
+    #   Возвращает окно заданного размера,
+    #   содержащее сконкатенированные окна
+    #   за определенное время суток.
+    #
+
+        # итерация по подпапкам данных каждого из радаров
+        for radar_subdir, _, _ in os.walk(self.datapath):
+
+            # итерация по периодам
+            for hour in range(0, 24, self.delta_hours):
+
+                # вытаскиваем все имена файлов за период указаный в переменной hour
+                filenames = self.__get_filenames(radar_subdir, hour)
+                # итерация скользящим окном по файлам
+                for i in range(len(filenames)-self.windows_num+1):
+                    
+                    # итерация по номерам каналов
+                    for channel in range(self.total_chs):
+
+                        # итерация по номерам лучей
+                        for beam in range(self.total_bms):
+
+                            # извлечение данных в виде многомерного массива
+                            yield self.__get_window(filenames=filenames[i:(i+self.windows_num)],
                                                     bmnum=beam,
                                                     channel=channel)
     
-    def __get_filenames(self, directory: str, hour: int):
+    def __get_filenames(self,
+                        directory: str, # папка, где лежат файлы
+                        hour: int):     # время суток
+
+        #
+        #   Возвращает упорядоченный список всех имен файлов,
+        #   относящихся к одному и тому же времени суток.
+        #   Пропущенные даты заменяются строкой-"заглушкой".
+        #
+
         formatted_time = dt.time(hour=hour, minute=0)
         filenames = sorted(glob.glob(directory+f'/*.{formatted_time.strftime("%H%M")}.00.*.fitacf.bz2'))
-        patches = [] # массив для пропущенных данных
-        # проверка остутствия пропусков во временном ряде
+        # массив для пропущенных значений
+        patches = []
+
+        # проверка остутствия пропусков во временном ряде и генерация заглушек
         if filenames:
+
             sliding_date = self.__get_date_from_filename(filenames[0])
             delta = dt.timedelta(days=1)
+
             for i in range(len(filenames)):
+
                 while self.__get_date_from_filename(filenames[i]) != sliding_date:
-                    warn(f'WARNING: There is no data for {hour} hours 0 minutes at {sliding_date} in {directory}')
+                    warn(f'There is no data for {hour} hours 0 minutes at {sliding_date} in {directory}!')
                     patches.append(f"{directory}/{sliding_date.strftime('%Y%m%d')}.patch")
                     sliding_date += delta
+
                 sliding_date += delta
+            
+            # cоединяем список файлов с заплатками
             filenames += patches
+
         return sorted(filenames)
 
     def __get_date_from_filename(self, filename):
+
+        #
+        #   Возвращает дату указанную в файле.
+        #
+
         return dt.datetime.strptime(filename.split('/')[-1].split('.')[0], '%Y%m%d')
 
-    def __get_data(self,
-                    filenames: list,
-                    bmnum: int,
-                    channel: int):
-        timeseries = np.zeros(shape=(self.nrang, 0, len(self.keys))) # сплошной массив для конкатенации всех периодов
-        mask = np.zeros(shape=(self.nrang, 0, 1)) # сплошной массив для конкатенации всех масок
+    def __get_window(self,
+                     filenames: list,
+                     bmnum: int,
+                     channel: int):
+
+        #
+        #   Функция конкатенирующая выводы функции __get_data
+        #   в большие, общие массивы.
+        #
+
+        timeseries = np.zeros(shape=(self.nrang, 0, len(self.keys)))
+        mask = np.zeros(shape=(self.nrang, 0, 1))
 
         for filename in filenames:
-            if filename.endswith('patch'):
-                timeseries = np.concatenate((timeseries,
-                                            np.zeros(shape=(self.nrang, self.reg_res, len(self.keys)))), axis=1)
-                mask = np.concatenate((mask,
-                                            np.zeros(shape=(self.nrang, self.reg_res, 1))), axis=1)
-            else:
-                with bz2.open(filename) as fp:
-                    fitacf_stream = fp.read()
-                reader = pydarn.SuperDARNRead(fitacf_stream, True)
-                records = reader.read_fitacf()
+            sub_series, sub_mask = self.__get_data(filename, bmnum, channel)
+            timeseries = np.concatenate((timeseries, sub_series), axis=1)
+            mask = np.concatenate((mask, sub_mask), axis=1)
 
-                if self.nrang != records[0]['nrang']:
-                    warn('WARNING: The specified nrang does not match the nrang in the file!')
+        return (timeseries, mask)
+    
+    def __get_data(self,
+                   filename: str,   # имя файла из которого извлекаются данные
+                   bmnum: int,      # номер луча
+                   channel: int):   # номер канала
+        
+        #
+        #   Извлекает из файла массив данных.
+        #
 
-                aux_timeseries = np.zeros(shape=(self.nrang, 0, len(self.keys)))
-                aux_mask = np.zeros(shape=(self.nrang, 0, 1))
+        print(filename)
 
-                for i in range(len(records)):
-                    if records[i]['bmnum'] == 0 and records[i]['channel'] == 0:
-                        line = np.zeros(shape=(self.nrang, 1, len(self.keys)))
-                        mask_line = np.zeros(shape=(self.nrang, 1, 1))
-                        try:
-                            for n, m in enumerate(records[i]['slist']):
-                                for j, key in enumerate(self.keys):
-                                    line[m, 0, j] = records[i][key][n]
-                                mask_line[m, 0] = records[i]['qflg'][n]
+        timeseries = np.zeros(shape=(self.nrang, self.reg_res, len(self.keys)))
+        mask = np.zeros(shape=(self.nrang, self.reg_res, 1))
 
-                            aux_timeseries = np.concatenate((aux_timeseries, line), axis=1)
-                            aux_mask = np.concatenate((aux_mask, mask_line), axis=1)
+        # если вместо записи присутствует заплатка, возвращаем пустые массивы
+        if filename.endswith('patch'):
+            return (timeseries, mask)
 
-                        except KeyError:
+        # открытие файла на чтение
+        with bz2.open(filename) as fp:
+            fitacf_stream = fp.read()
+        reader = pydarn.SuperDARNRead(fitacf_stream, True)
+        records = reader.read_fitacf()
 
-                            aux_timeseries = np.concatenate((aux_timeseries, line), axis=1)
-                            aux_mask = np.concatenate((aux_mask, mask_line), axis=1)
+        # список меток времени
+        timestamps = []
+        # счетчик, показывающий текущий индекс в массивах timeseries и mask
+        cur = 0
 
-                if self.reg_res != aux_timeseries.shape[1]:
-                    # TODO: как интерполировать значения от n>60 до 60?
-                    warn(f'The specified regular resolution does not match the regular in {filename}, which is {aux_timeseries.shape[1]}')
+        for i in range(len(records)):
+
+            # если номер луча и канала совпадают с нужным
+            if records[i]['bmnum'] == bmnum and records[i]['channel'] == channel:
                 
-                timeseries = np.concatenate((timeseries, aux_timeseries), axis=1)
-                mask = np.concatenate((mask, aux_mask), axis=1)
+                rec_time = dt.datetime(year=records[i]['time.yr'],
+                        month=records[i]['time.mo'],
+                        day=records[i]['time.dy'],
+                        hour=records[i]['time.hr'],
+                        minute=records[i]['time.mt'])
 
-                print(f'timeseries shape:{timeseries.shape}')
-                print(f'mask shape:{mask.shape}\n')
+                if timestamps != []:
 
-        return (timeseries, mask)        
+                    diff_mins = (rec_time - timestamps[-1]).seconds // 60
 
-loader = DataLoader('./data', 75, ['p_l', 'pwr0', 'v'], 16, 1)
+                    if diff_mins == 2:
+                        timestamps.append(rec_time)
+                        if 'slist' in records[i]:
+                            for n, m in enumerate(records[i]['slist']):
+                                for j, k in enumerate(self.keys):
+                                    timeseries[m, cur, j] = records[i][k][n]
+                                mask[m, cur] = records[i][self.mask_key][n]
+                        cur += 1
+
+                    elif diff_mins == 1:
+                        continue
+                    # TODO: могут ли быть другие случаи?
+                    else:
+                        break
+
+                else:
+
+                    timestamps.append(rec_time)
+                    cur += 1
+        
+        # предупреждение о пробелах в данных
+        if cur != self.reg_res:
+            warn(f'Got only {cur} records from {filename}!')
+
+        return (timeseries, mask)
+
+loader = DataLoader('./data', 30)
 for file in loader:
     pass
