47f135e5dbbb87243544a8d63f11e76ee611ccab
Буда Вампилов <budavampilov@MacBook-Air-Buda.local>
2022-10-25 22:02:08 +0800

    переписал DataLoader

--- DataLoader.py
+++ DataLoader.py
@@ -1,9 +1,11 @@
 import os
 import random
+from time import time
 import numpy as np
 import datetime
 import glob
 import warnings
+from tqdm import tqdm
 
 class DataLoader:
 
@@ -16,15 +18,43 @@ class DataLoader:
         self.windows_num = windows_num  # количество периодов в окне
         self.shuffle = shuffle
         
-        self.sequence_for_learning = self.__get_sequence()
+        self.sequence_for_learning = self.__get_sequence_for_learning()
 
         if self.shuffle:
             random.shuffle(self.sequence_for_learning)
+        
+        for filename in self.sequence_for_learning[0]:
+            if filename.endswith('patch'):
+                continue
+
+            x = np.load(filename)
+            self.shape = x.shape
+            break
 
     def __iter__(self):
-        pass
+        for sequence in self.sequence_for_learning:
+            yield self.__xy(sequence)
+        if self.shuffle:
+            random.shuffle(self.sequence_for_learning)
 
-    def __get_sequence(self, augmentation_step=1):
+    def __xy(self, sequence):
+        if len(sequence) < 2:
+            raise('Learning sequence must be at least 2 elements!')
+        
+        timeseries = []
+
+        for filename in sequence:
+            if filename.endswith('patch'):
+                timeseries.append(np.zeros(shape=self.shape))
+            else:
+                timeseries.append(np.load(filename))
+
+        x = np.concatenate(timeseries[:-1], axis=1)
+        y = timeseries[-1]
+        
+        return x, y
+
+    def __get_sequence_for_learning(self, augmentation_step=1):
 
     #
     #   Генерирует последовательности имен файлов для обучения,
@@ -55,7 +85,7 @@ class DataLoader:
                     
                     # хотя бы половина контекстных данных должны присутствовать
                     sub_sequence = filenames[i:i+self.windows_num+1] # последний элемент - целевое значение
-                    if self.__count_patches(sub_sequence) > len(sub_sequence) / 2:
+                    if self.__count_patches(sub_sequence) > len(sub_sequence) / 4:
                         continue
                     
                     sequence.append(sub_sequence)
@@ -116,4 +146,9 @@ class DataLoader:
 warnings.filterwarnings('ignore')
 
 loader = DataLoader('./converted')
-print(len(loader.sequence_for_learning))
+start_time = datetime.datetime.now()
+for _ in tqdm(loader):
+    
+    pass
+
+print(datetime.datetime.now() - start_time)
