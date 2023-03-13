916f86c219c871e198b83229d1f038e29421bfac
Буда Вампилов <39209065+kupaqu@users.noreply.github.com>
2023-03-13 15:19:48 +0800

    Update DataConverter.py

--- DataConverter.py
+++ DataConverter.py
@@ -5,121 +5,129 @@ import bz2
 import pydarnio
 import datetime
 import warnings
-
 import sys
 
 class DataConverter:
-    def __init__(self, src_dir: str, dst_dir: str, nrang=70, reg_res=60, bmnum=16, chnum=1, keys=['pwr0', 'v', 'p_l', 'p_s', 'w_l', 'w_s', 'qflg']):
+
+    def __init__(self,
+                 src_dir: str,  # папка с исходными данными
+                 dst_dir: str,  # папка с конвертированными данными
+                 nrang=100,     # количество гейтов
+                 reg_res=60,    # выходное разрешение
+                 bmnum=16,      # количество лучей
+
+                 keys=['pwr0',  # Lag zero power (actually SNR), estimated from voltage samples (not fitted)
+                       'qflg',  # Quality of fit flag for ACF
+                       'p_l',   # Power (actually SNR) from lambda fit of ACF
+                       'p_s',   # Power (actually SNR) from sigma fit of ACF
+                       'v',     # Velocity from fit of ACF
+                       'w_l',   # Spectral width from lambda fit of ACF
+                       'w_s']): # Spectral width from sigma fit of ACF
+        
+        # разрешение радара, у CVW – раз в минуту
+        self.radar_res = 120
+
+        # абсолютный путь до папки с исходными данными
         self.src_dir = os.path.abspath(src_dir)
+
+        # абсолютный путь до папки с конвертированными данными
         if not os.path.isdir(dst_dir):
             os.mkdir(dst_dir)
         self.dst_dir = os.path.abspath(dst_dir)
+
+        # индекс последнего разделителя в абсолютном пути до папки с исходными данными
         self.src_prefix = len(self.src_dir) + len(os.path.sep)
 
         self.nrang = nrang
         self.reg_res = reg_res
+        self.bmnum = bmnum
         self.keys = keys
 
-        self.bmnum = bmnum
-        self.chnum = chnum
-
+    # копирует структуру папки с исходными данными в папку с конвертированными данными
     def __copy_dir_tree(self):
+
         for root, dirs, _ in os.walk(self.src_dir):
             for dir in dirs:
-                # индекс последнего разделителя относительно
-                # полного пути до исходной папки
                 dirpath = os.path.join(self.dst_dir, root[self.src_prefix:], dir)
                 try:
                     os.mkdir(dirpath)
                 except FileExistsError:
                     continue
     
+
     def convert(self):
-        self.__copy_dir_tree()
+
+        self.__copy_dir_tree() # создаем структуру папки
+
+        # итерация по папкам месяцов
         for root, _, files in os.walk(self.src_dir):
             if len(files) > 0:
                 print(f'Converting FITACF\'s to NumPy arrays at {root}')
                 for file in tqdm(files):
 
+                    # название исходного файла
                     src_filename = os.path.join(root, file)
                     if not src_filename.endswith('fitacf.bz2'):
                         continue
-
-                    dst_filename = os.path.join(self.dst_dir, root[self.src_prefix:], file)[:-len('fitacf.bz2')]
-                    is_valid, content = self.__get_content(src_filename)
-                    if is_valid:
-                        np.save(dst_filename+'npy', content)
-                    else:
-                        continue
-    
-    def __get_content(self, filename):
-        timeseries = np.zeros(shape=(self.nrang, self.reg_res, len(self.keys), self.bmnum, self.chnum))
-
-        # открытие файла на чтение
-        with bz2.open(filename) as fp:
-            fitacf_stream = fp.read()
-
-        try:
-            reader = pydarnio.SDarnRead(fitacf_stream, True)
-            records = reader.read_fitacf()
-
-            for beam in range(self.bmnum):
-
-                for channel in range(self.chnum):
-
-                    # список меток времени
-                    timestamps = []
-
-                    # счетчик, показывающий текущий индекс в массивах timeseries и mask
-                    cur = 0
-
-                    for i in range(len(records)):
-
-                        # если номер луча и канала совпадают с нужным
-                        if records[i]['bmnum'] == beam and records[i]['channel'] == channel:
-                            
-                            rec_time = datetime.datetime(year=records[i]['time.yr'],
-                                                        month=records[i]['time.mo'],
-                                                        day=records[i]['time.dy'],
-                                                        hour=records[i]['time.hr'],
-                                                        minute=records[i]['time.mt'])
-
-                            if timestamps != []:
-                                diff_mins = round((rec_time - timestamps[-1]).seconds / 60)
-
-                            else:
-                                diff_mins = 0
-
-                            if diff_mins % 2:
-                                continue
-                            
-                            else:
-                                timestamps.append(rec_time)
-                                cur += diff_mins // 2
-                                
-                                if cur >= self.reg_res:
-                                    break
-
-                                if 'slist' in records[i]:
-                                    for n, m in enumerate(records[i]['slist']):
-                                        if m >= self.nrang:
-                                            continue
-                                        for j, k in enumerate(self.keys):
-                                            timeseries[m, cur, j, beam, channel] = records[i][k][n]
                     
-                    # предупреждение о пробелах в данных
-                    if cur < self.reg_res-1:
-                        # warnings.warn(f'Got only {cur} records from {filename} at beam {beam}, channel {channel}!')
-                        print(f'Got only {cur} records from {filename} at beam {beam}, channel {channel}!')
-        
-        except pydarnio.exceptions.dmap_exceptions.EmptyFileError:
-            return (False, timeseries)
-        
-        if np.count_nonzero(timeseries[:,:,-1,:,:]) == 0:
-            return (False, timeseries)
-        
-        return (True, timeseries)
+                    # название конвертированного файла
+                    dst_filename = os.path.join(self.dst_dir, root[self.src_prefix:], file)[:-len('.fitacf.bz2')]
 
+                    # открытие исходного файла на чтение
+                    with bz2.open(src_filename) as fp:
+                        fitacf_stream = fp.read()
+                        try:
+                            reader = pydarnio.SDarnRead(fitacf_stream, True)
+                            records = reader.read_fitacf()
+
+                            # итерация по лучам. сохраняем в отдельные файлы, т.к. на некоторых лучах могут отсутствовать наблюдения
+                            for beam in range(self.bmnum):
+                                is_valid, content = self.__get_content(records, beam)
+                                if is_valid:
+                                    np.save(dst_filename + f'.{beam}' + '.npy', content)
+
+                        except pydarnio.exceptions.dmap_exceptions.EmptyFileError:
+                            continue
+    
+    def __get_content(self, records, beam):
+
+            timeseries = np.zeros(shape=(self.nrang, self.radar_res, len(self.keys))) # разметка пустого массива для заполнения
+            timestamps = []
+
+            for record in records:
+
+                # отбор данных по лучу
+                if record['bmnum'] == beam and record['channel'] == 0:
+
+                    # метка по времени, которая используется для расчета индекса
+                    timestamps.append(datetime.datetime(year=record['time.yr'],
+                                                        month=record['time.mo'],
+                                                        day=record['time.dy'],
+                                                        hour=record['time.hr'],
+                                                        minute=record['time.mt']))
+                    
+                    # индекс наблюдения в массиве
+                    if timestamps:
+                        index = (timestamps[-1] - timestamps[0]).seconds // 60
+                    else:
+                        index = 0
+
+                    # конвертация
+                    if 'slist' in record:
+                        for gate_index, gate in enumerate(record['slist']):
+                            for key_index, key in enumerate(self.keys):
+                                if key == 'pwr0':
+                                    timeseries[:, index, key_index] = record[key]
+                                else:
+                                    timeseries[gate, index, key_index] = record[key][gate_index]
+
+            # если не было ни одного наблюдения
+            if len(timestamps) == 0:
+                return (False, None)
+
+            # ужатие до reg_res
+            return (True, timeseries[:, ::self.radar_res//self.reg_res])
+        
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
 
